37f9be3abc299a6525106fd724b7eecc63b598b3
your_name <tetianabeliak2024@gmail.com>
2024-09-14 11:45:37 +0300

    1

---
+++ js/task-3.js
@@ -0,0 +1,31 @@
+const profile = {
+    username: "Jacob",
+  playTime: 300,
+
+    changeUsername(newName) {
+        if (this.username !== newName) {
+            this.username = newName;
+        }
+        console.log(this);
+    },
+    updatePlayTime(hours) {
+        this.playTime += hours;
+    console.log(this);
+    },
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    }
+
+};
+    
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile);
+
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
+
+
